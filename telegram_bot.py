a6f7bd995d56c3c9c0d54f8403bb1ded551b8587
LaryBary174 <lotuc74@gmail.com>
2025-01-14 11:55:39 +0500

    исправление замечаний

--- telegram_bot.py
+++ telegram_bot.py
@@ -1,25 +1,23 @@
+import time
+
 import telegram
 import requests
 from environs import Env
 
 
 def get_notifications(url, headers):
-    try:
-        response = requests.get(url, headers=headers)
-        response.raise_for_status()
-        if response.json().get('status') == 'timeout':
-            params = {
-                'timestamp': response.json().get('timestamp_to_request'),
-            }
-            new_response = requests.get(url, headers=headers, params=params)
-            new_response.raise_for_status()
-            return new_response.json()
-        else:
-            return response.json()
-    except requests.exceptions.ReadTimeout:
-        print('Ошибка времени ожидания')
-    except ConnectionError:
-        print('Сбой соединения!')
+    response = requests.get(url, headers=headers)
+    response.raise_for_status()
+    notifications = response.json()
+    if notifications['status'] == 'timeout':
+        params = {
+            'timestamp': notifications['timestamp_to_request'],
+        }
+        repeated_response = requests.get(url, headers=headers, params=params)
+        repeated_response.raise_for_status()
+        return repeated_response.json()
+    else:
+        return response.json()
 
 
 def main():
@@ -37,20 +35,24 @@ def main():
             notifications = get_notifications(url, headers)
             if notifications['status'] == 'found':
                 for notification in notifications['new_attempts']:
-                    if notification['is_negative']:
+                    message = 'НЕ ПРИНЯТ' if notification['is_negative'] else 'ПРИНЯТ'
+                    bot.send_message(chat_id=chat_id,
+                                     text=f'<a href="{notification["lesson_url"]}">Урок {notification["lesson_title"]} </a> {message}.',
+                                     parse_mode=telegram.ParseMode.HTML)
 
-                        bot.send_message(chat_id=chat_id,
-                                         text=f'<a href="{notification["lesson_url"]}">Урок {notification["lesson_title"]} </a> НЕ ПРИНЯТ.',
-                                         parse_mode=telegram.ParseMode.HTML)
 
-                    else:
-                        bot.send_message(chat_id=chat_id,
-                                         text=f'<a href="{notification["lesson_url"]}">Урок {notification["lesson_title"]}</a> ПРИНЯТ.',
-                                         parse_mode=telegram.ParseMode.HTML)
+        except requests.exceptions.ReadTimeout:
+            print('Повтор запроса')
+            continue
+        except ConnectionError:
+            print('Сбой соединения!')
         except telegram.error.TelegramError:
             print('Ошибка!')
+            time.sleep(10)
         except telegram.error.NetworkError:
             print('Ошибка подключения')
+            time.sleep(10)
+
 
 if __name__ == '__main__':
     main()
